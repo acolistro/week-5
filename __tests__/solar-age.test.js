df1d0e14e7fc5f8763e6574f7d233819583366ff
Alyssa Colistro <arcolistro@gmail.com>
2019-11-18 09:53:49 -0800

    Add test to determine whether the age entered is negative.

--- __tests__/solar-age.test.js
+++ __tests__/solar-age.test.js
@@ -1,9 +1,14 @@
-import { age } from './../src/solar-age.js';
+import { Age } from './../src/solar-age.js';
 
 describe('Age', () => {
 
-    test('should correctly create an age object that takes a positive number', () => {
+    test('should correctly create an age object that takes a number', () => {
       var age = new Age(25);
       expect(age.earthAge).toEqual(25);
+    });
+
+    test('should correctly determine whether Age is negative', () => {
+      var negativeAge = new Age(-20);
+      expect(negativeAge.negativeCheck()).toEqual("age is negative")
     });
 });
