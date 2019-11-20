52360a28bbcfdb3444fc0afcef59106fb680a26f
Student <example@example.com>
2019-11-20 12:50:08 -0800

    Create test branch to test new code refactor.

--- __tests__/solar-age.test.js
+++ __tests__/solar-age.test.js
@@ -1,20 +1,44 @@
-import { Age } from './../src/solar-age.js';
+import { Age } from '../src/solar-age.js';
 
 describe('Age', () => {
 
     test('should correctly create an age object that takes a number', () => {
-      var age = new Age(25);
+      const age = new Age(25);
       expect(age.earthAge).toEqual(25);
     });
 
     test('should correctly determine whether Age is negative', () => {
-      var negativeAge = new Age(-20);
+      const negativeAge = new Age(-20);
       expect(negativeAge.negativeCheck()).toEqual("age is negative")
     });
 
-    test('should convert age to solar age', () => {
-      var age = new Age(25)
-      var mercuryAge = age.toSolar();
+    test('should convert age to mercury age', () => {
+      const age = new Age(25)
+      const mercuryAge = age.toMercury();
       expect(mercuryAge).toEqual(25 / 0.24);
+    });
+
+    test('should convert age to venus age', () => {
+      const age = new Age(25)
+      const venusAge = age.toVenus();
+      expect(venusAge).toEqual(25 / 0.62);
+    });
+
+    test('should convert age to mars age', () => {
+      const age = new Age(25)
+      const marsAge = age.toMars();
+      expect(marsAge).toEqual(25 / 1.88);
+    });
+
+    test('should convert age to jupiter age', () => {
+      const age = new Age(25)
+      const jupiterAge = age.toJupiter();
+      expect(jupiterAge).toEqual(25 / 11.86);
+    });
+
+    test('should determine years left based on life expectancy', () => {
+      const age = new Age(25)
+      const lifeLeft = age.lifeLeft();
+      expect(lifeLeft).toEqual(75 - 25);
     });
 });
--- src/solar-age.js
+++ src/solar-age.js
@@ -1,14 +1,41 @@
-export function Age(earthAge, mercuryAge) {
+export class Age {
+  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge, lifeLeft) {
   this.earthAge = earthAge;
   this.mercuryAge = mercuryAge;
-};
+  this.venusAge = venusAge;
+  this.marsAge = marsAge;
+  this.jupiterAge = jupiterAge;
+  this.lifeLeft = lifeLeft;
+}
 
 Age.prototype.negativeCheck = function() {
   if (this.earthAge < 0) {
     return "age is negative";
   }
-};
+}
 
-Age.prototype.toSolar = function() {
+Age.prototype.toMercury = function() {
   this.mercuryAge = (this.earthAge / 0.24);
+  return this.mercuryAge;
+}
+
+Age.prototype.toVenus = function() {
+  this.venusAge = (this.earthAge / 0.62);
+  return this.venusAge;
+}
+
+Age.prototype.toMars = function() {
+  this.marsAge = (this.earthAge / 1.88);
+  return this.marsAge;
+}
+
+Age.prototype.toJupiter = function() {
+  this.jupiterAge = (this.earthAge / 11.86);
+  return this.jupiterAge;
+}
+
+Age.prototype.lifeLeft = function() {
+  this.lifeLeft = (75 - this.earthAge);
+  return this.lifeLeft;
+}
 };
